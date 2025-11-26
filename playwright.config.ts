003761cd92f1665b17ef3d3f239d2249f991c12b
mikedane-bitovi <mikedane@bitovi.com>
2025-11-26 14:21:29 -0500

    added global teardown after e2e tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
     testDir: 'tests/e2e',
     globalSetup: require.resolve('./tests/e2e/global-setup.js'),
+    globalTeardown: require.resolve('./tests/e2e/global-teardown.js'),
     timeout: 30 * 1000,
     expect: { timeout: 5000 },
     fullyParallel: true,
